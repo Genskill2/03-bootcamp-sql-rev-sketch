8479b061114b6ca39f64c35a08874a78bbeb68ac
rev-sketch <85095179+rev-sketch@users.noreply.github.com>
2021-06-19 17:21:37 +0530

    query4

--- query4.sql
+++ query4.sql
@@ -1,3 +1,1 @@
-SELECT s.name 
-FROM subjects s,books b,books_subjects bs 
-WHERE bs.book = b.id and bs.subject = s.id and b.title = "Atomic Habits";
+select s.name from subjects s,books b,books_subjects bs where bs.book = b.id and bs.subject = s.id and b.title = "Atomic Habits";
